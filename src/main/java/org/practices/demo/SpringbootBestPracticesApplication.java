57fe00fae350a8bf74e94c7cc3898b40fe5a21ef
Xinguang XG1 Duan <duanxg1@lenovo.com>
2021-04-11 22:39:59 +0800

    add codes

--- src/main/java/org/practices/demo/SpringbootBestPracticesApplication.java
+++ src/main/java/org/practices/demo/SpringbootBestPracticesApplication.java
@@ -1,9 +1,13 @@
 package org.practices.demo;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
+//@Configurable
 public class SpringbootBestPracticesApplication {
 
 	public static void main(String [] args) {
